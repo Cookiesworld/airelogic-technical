977b520d5cf84a096c83b81e979a6f4ab8bfedcd
John Cooke <jcooke@johncooke.info>
2022-03-22 14:50:13 +0000

    tweaks needs update

--- airelogic-client-app/src/app/services/bug.service.ts
+++ airelogic-client-app/src/app/services/bug.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { timingSafeEqual } from 'crypto';
-import { Bugmodel } from '../model/bugmodel';
 
 @Injectable({
   providedIn: 'root'
